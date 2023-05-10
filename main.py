2523d55e2f4374917de9b296bfd0d2b95a3ce185
Ben <38230235+KBenster@users.noreply.github.com>
2023-05-10 15:19:35 -0600

    Following tutorial and documentation from tensorflow to understand rnns better

--- main.py
+++ main.py
@@ -26,12 +26,64 @@ train_dataset, test_dataset = dataset['train'], dataset['test']
 #  TensorSpec(shape=(), dtype=tf.int64, name=None))
 
 
-# for example, label in train_dataset.take(1):
-#   print('text: ', example.numpy())
-#   print('label: ', label.numpy())
+for example, label in train_dataset.take(1):
+  print('text: ', example.numpy())
+  print('label: ', label.numpy())
 
 BUFFER_SIZE = 10000
 BATCH_SIZE = 64
 
-https://www.tensorflow.org/text/tutorials/text_classification_rnn
-https://www.tensorflow.org/api_docs/python/tf/data/Dataset#methods_2
+train_dataset = train_dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE).prefetch(tf.data.AUTOTUNE)
+test_dataset = test_dataset.batch(BATCH_SIZE).prefetch(tf.data.AUTOTUNE)
+
+for example, label in train_dataset.take(1):
+  print('texts: ', example.numpy()[:3])
+  print()
+  print('labels: ', label.numpy()[:3])
+
+
+#The adapt method sets the layer's vocabulary
+VOCAB_SIZE = 1000
+encoder = tf.keras.layers.TextVectorization(
+    max_tokens=VOCAB_SIZE)
+encoder.adapt(train_dataset.map(lambda text, label: text))
+
+
+vocab = np.array(encoder.get_vocabulary())
+print(vocab[:20])
+
+encoded_example = encoder(example)[:3].numpy()
+print(encoded_example)
+
+for n in range(3):
+  print("Original: ", example[n].numpy())
+  print("Round-trip: ", " ".join(vocab[encoded_example[n]]))
+  print()
+     
+model = tf.keras.Sequential([
+    encoder,
+    tf.keras.layers.Embedding(
+        input_dim=len(encoder.get_vocabulary()),
+        output_dim=64,
+        # Use masking to handle the variable sequence lengths
+        mask_zero=True),
+    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64)),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(1)
+])
+
+#print([layer.supports_masking for layer in model.layers])
+
+
+
+model.compile(loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+              optimizer=tf.keras.optimizers.Adam(1e-4),
+              metrics=['accuracy'])
+
+#train the model...
+
+# ipynb version
+# https://github.com/tensorflow/text/blob/master/docs/tutorials/text_classification_rnn.ipynb
+
+# https://www.tensorflow.org/text/tutorials/text_classification_rnn
+# https://www.tensorflow.org/api_docs/python/tf/data/Dataset#methods_2
