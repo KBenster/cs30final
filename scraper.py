20ba92073d83dd557d4cbbe4a22dd781b27697ac
Ben <38230235+KBenster@users.noreply.github.com>
2023-05-31 15:19:19 -0600

    scrape

--- scraper.py
+++ scraper.py
@@ -1,5 +1,10 @@
 import praw
 #Mozilla/5.0 (Nintendo WiiU) AppleWebKit/536.30 (KHTML, like Gecko) NX/3.0.4.2.12 NintendoBrowser/4.3.1.11264.US
 
-#go through wallstreetbets and other financial reddit sites. get the stock tickers from the title and comments, read title, the text and
-#top 5 comments to gain overall sentiment. 
+#check all titles for stock tickers, get overall sentiment from comments and text.
+
+reddit = praw.Reddit(
+    client_id="my client id",
+    client_secret="my client secret",
+    user_agent="my user agent",
+)
