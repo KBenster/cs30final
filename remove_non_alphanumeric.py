e8f638d7c06f5da1d25275639441d57293c91d5b
Ben <38230235+KBenster@users.noreply.github.com>
2023-06-08 13:06:10 -0600

    forgot

--- remove_non_alphanumeric.py
+++ remove_non_alphanumeric.py
@@ -4,3 +4,5 @@ with io.open('datasets/twitter.csv','r',encoding='latin-1',errors='ignore') as i
      io.open('datasets/twitter_parsed.csv','w',encoding='ascii',errors='ignore') as outfile:
     for line in infile:
         print(*line.split(), file=outfile)
+
+#https://stackoverflow.com/questions/26369051/python-read-from-file-and-remove-non-ascii-characters
